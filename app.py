d854267a3a44ef4fd142885900f125d37c62e4b2
Kendall <kdmccroy@gmail.com>
2021-12-30 03:52:39 -0500

    station query

--- app.py
+++ app.py
@@ -62,6 +62,31 @@ def precipitation():
     preicpData = list(np.ravel(results))
     return jsonify(preicpData)
 
+################################################
+#Station
+################################################
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    #Query for stations
+    stations = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
+
+    session.close()
+
+# Create a dictionary to convert the query results
+    all_stations = []
+    for station, name, latitude, longitude, elevation in stations:
+        station_dict = {}
+        station_dict["station"] = station
+        station_dict["name"] = name
+        station_dict["latitude"] = latitude
+        station_dict["longitude"] = longitude
+        station_dict["elevation"] = elevation
+        all_stations.append(station_dict)
+
+        return jsonify(all_stations)
+
 # Step 6 Define what to do when a user hits the /about route
 @app.route("/about")
 def about():
